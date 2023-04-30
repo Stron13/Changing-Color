918bac5953f369f6a7ee6e959b9dbd33bc081ff1
Stron13 <131529490+Stron13@users.noreply.github.com>
2023-04-30 11:34:40 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var box ;
+function setup() {
+  box = createSprite(200,200,30,30);
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background(30);
+  if(keyIsDown(RIGHT_ARROW)){
+   background("red");
+    box.position.x=box.position.x+5
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    background("yellow")
+    box.position.x=box.position.x-5
+  }
+  if(keyIsDown(UP_ARROW)){
+    box.position.y=box.position.y-5
+  }
+if(keyIsDown(DOWN_ARROW)){
+  box.position.y=box.position.y+5
+}
+drawSprites()
+}
+
+
+
+
